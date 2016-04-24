6f9deecc2e4978dbd2973f35a2e5a24b6c6f07c6
Harry Ng <harryworld@gmail.com>
2016-04-24 21:19:36 +0800

    Initial Commit.

---
+++ RxSwiftMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RxSwiftMac
+//
+//  Created by Harry Ng on 24/4/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
